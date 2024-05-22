87136535500562470be4ad36b8344e2e7e59a391
Simonas Sabaliauskas <sim.sabaliauskas@gmail.com>
2024-05-22 12:23:13 +0300

    Ikelta Uzduotis01, netestuota daug.

--- Uzduotis01/Program.cs
+++ Uzduotis01/Program.cs
@@ -1,18 +1,17 @@
 ﻿namespace Uzduotis01
 {
+    // Užduotis: Sukurti programą, kuri leis vartotojui įvesti filmų sąrašą ir jų reitingus, o po to atliks įvairias operacijas su šiais duomenimis.
+    // Sukurkite konsolinę programą, kuri leis vartotojui įvesti n filmų pavadinimus ir jų reitingus(nuo 1 iki 10).
+    // Duomenis saugokite sąraše ar masyve.
+    // Leiskite vartotojui pasirinkti iš meniu, ką jis nori atlikti su įvestais duomenimis:
+    // a.Rodyti visus filmus ir jų reitingus.
+    // b.Rodyti tik tuos filmus, kurių reitingas didesnis nei nurodyta vertė.
+    // c.Rasti filmą pagal pavadinimą ir parodyti jo reitingą.
+    // d.Atnaujinti filmo reitingą.
+    // e.Ištrinti filmą iš sąrašo.
+    // f.Išeiti iš programos.
     internal class Program
     {
-        // Užduotis: Sukurti programą, kuri leis vartotojui įvesti filmų sąrašą ir jų reitingus, o po to atliks įvairias operacijas su šiais duomenimis.
-        // Sukurkite konsolinę programą, kuri leis vartotojui įvesti n filmų pavadinimus ir jų reitingus(nuo 1 iki 10).
-        // Duomenis saugokite sąraše ar masyve.
-        // Leiskite vartotojui pasirinkti iš meniu, ką jis nori atlikti su įvestais duomenimis:
-        // a.Rodyti visus filmus ir jų reitingus.
-        // b.Rodyti tik tuos filmus, kurių reitingas didesnis nei nurodyta vertė.
-        // c.Rasti filmą pagal pavadinimą ir parodyti jo reitingą.
-        // d.Atnaujinti filmo reitingą.
-        // e.Ištrinti filmą iš sąrašo.
-        // f.Išeiti iš programos.
-
         private static Filmoteka filmoteka = new();
 
         public static void Main(string[] args)
